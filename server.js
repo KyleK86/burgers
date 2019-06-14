cb66606393a0bd818b19549c28c30eccdf9d605d
Kyle Knox <kyleknox86@gmail.com>
2019-06-14 15:39:01 -0600

    server update

--- server.js
+++ server.js
@@ -17,15 +17,16 @@ app.use(express.json());
 // Set Handlebars.
 var exphbs = require("express-handlebars");
 require("./config/connection.js")
-// app.engine("handlebars", exphbs({
-//     defaultLayout: "main"
-// }));
-// app.set("view engine", "handlebars");
+
+app.engine("handlebars", exphbs({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-// var routes = require("./controllers/burgersController.js");
+var routes = require("./controllers/burgersController.js");
 
-// app.use(routes);
+app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function () {
