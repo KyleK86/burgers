49cad9df0ca26f216682950a46cac488e4739e7f
Kyle Knox <kyleknox86@gmail.com>
2019-06-13 20:11:14 -0600

    connect

---
+++ db/seeds.sql
@@ -0,0 +1,24 @@
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Whopper');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Big Mac');
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Baconator', true);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Royal Robin', true);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Jumbo Jack', true);
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Thickburger');
